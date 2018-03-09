db4a02ace5cf49f9e32abfeaccfb77b28be0f47e
Emmanuel <esowoeye@gmail.com>
2018-03-10 00:57:11 +0100

    First version of application moved into github

---
+++ ASDL/wsgi.py
@@ -0,0 +1,24 @@
+"""
+WSGI config for ASDL project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ASDL.settings")
+
+application = get_wsgi_application()
+
+
+#Add static serving using whitenoise
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
